ba60d906d8b22ffe4285defaab11a5fcf051f67e
ravivish <ravivishwakarma825@gmail.com>
2019-04-15 21:02:55 +0530

    removed unecessary statements

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -29,13 +29,13 @@ const forcast = (longitude,lattitude,callback) => {
 
     const url = 'https://api.darksky.net/forecast/150d57a07eb94dfc3e013a1d8ea8ac0a/'+lattitude+','+longitude+'?unit=si'
 
-    request({url:url , json:true}, (error,{ body }) => {
+    request( {url:url , json:true}, (error, { body } ) => {
         if(error){
             callback('Unable to connect wheather services!', undefined)
         }else if(body.error){
             callback('Unable to find location!',undefined)
         }else{
-            console.log(body.daily.data[0])
+            
             // fohrenheit = response.body.currently.temperature
             // celsius = ((fohrenheit - 32) * 5 / 9).toFixed(2)            
             celsius = ((body.currently.temperature - 32) * 5 / 9).toFixed(2) //tofixed(2) for 2 digit after decimal
